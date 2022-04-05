29b96b68434dc44b84482323f190b49b49f2d49c
Bartek5756 <101180607+Bartek5756@users.noreply.github.com>
2022-04-05 09:28:19 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,16 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println("choinka");
+    int w=5;
+
+  for (int i=w; i > 0;i--)
+    {
+    for (int j=i; j >0;j--)
+      System.out.print(" ");
+    for (int k=2*(w - i)+1; k > 0; k--)  
+
+      System.out.print("*");
+      System.out.println();
+    }
+  }
+}
